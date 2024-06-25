7ae7251d83cd5861fddff2ec9a5ba5f063025b5b
Tóth Szabolcs Balázs <szabolcs978@gmail.com>
2024-06-25 20:32:43 +0100

    I will need to get the initial rotation too.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// JavaScript
 const card1 = document.querySelector(".card1");
+const hand = document.querySelector(".hand");
+const handX = hand.getBoundingClientRect().x;
+const handY = hand.getBoundingClientRect().y;
+
 let isDragging = false;
 let initialX, initialY;
 
 card1.addEventListener("mousedown", (e) => {
   isDragging = true;
-  console.log(e.clientX);
-  console.log(card1.getBoundingClientRect().x);
+  console.log(card1.getBoundingClientRect());
   initialX = card1.getBoundingClientRect().x;
   initialY = card1.getBoundingClientRect().y;
   card1.style.cursor = "grabbing";
@@ -16,14 +18,15 @@ document.addEventListener(
   "mousemove",
   throttle((e) => {
     if (!isDragging) return;
-    const newX = e.clientX;
-    const newY = e.clientY;
+    const newX = e.clientX - handX;
+    const newY = e.clientY - handY;
     card1.style.transform = `translate(${newX}px, ${newY}px)`;
   }, 16)
 ); // Throttle to ~60 FPS
 
 document.addEventListener("mouseup", () => {
   isDragging = false;
+  card1.style.transform = `translate(${0}px, ${0}px)`;
   card1.style.cursor = "grab";
 });
 
