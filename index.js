e7e551c5b2824f21990200374f5e2d899b90a4d0
Tóth Szabolcs Balázs <szabolcs978@gmail.com>
2024-06-26 19:12:15 +0100

    Getting the rotation in JS.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const card1 = document.querySelector(".card1");
+// Disable text selection for the entire page
+document.addEventListener("selectstart", (e) => {
+  e.preventDefault();
+});
+
+let grabbedCard = undefined;
+let initialRot = 0;
 const hand = document.querySelector(".hand");
 const handX = hand.getBoundingClientRect().x;
 const handY = hand.getBoundingClientRect().y;
@@ -6,28 +12,44 @@ const handY = hand.getBoundingClientRect().y;
 let isDragging = false;
 let initialX, initialY;
 
-card1.addEventListener("mousedown", (e) => {
+hand.addEventListener("mousedown", (e) => {
   isDragging = true;
-  console.log(card1.getBoundingClientRect());
-  initialX = card1.getBoundingClientRect().x;
-  initialY = card1.getBoundingClientRect().y;
-  card1.style.cursor = "grabbing";
+  const card = e.target;
+  grabbedCard = card;
+  const computedStyle = window.getComputedStyle(grabbedCard, null);
+  const matrix = computedStyle.getPropertyValue("transform");
+
+  let values = matrix.split("(")[1];
+  values = values.split(")")[0];
+  values = values.split(",");
+
+  const a = values[0];
+  const b = values[1];
+
+  const angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+  initialRot = angle;
+  console.log(angle);
+
+  console.log(values);
+  initialX = card.getBoundingClientRect().x;
+  initialY = card.getBoundingClientRect().y;
+  grabbedCard.style.cursor = "grabbing";
 });
 
 document.addEventListener(
   "mousemove",
   throttle((e) => {
     if (!isDragging) return;
-    const newX = e.clientX - handX;
-    const newY = e.clientY - handY;
-    card1.style.transform = `translate(${newX}px, ${newY}px)`;
-  }, 16)
+    const newX = e.clientX - handX - 30;
+    const newY = e.clientY - handY - 40;
+    grabbedCard.style.transform = `translate(${newX}px, ${newY}px)`;
+  }, 8)
 ); // Throttle to ~60 FPS
 
 document.addEventListener("mouseup", () => {
   isDragging = false;
-  card1.style.transform = `translate(${0}px, ${0}px)`;
-  card1.style.cursor = "grab";
+  grabbedCard.style.transform = `translate(${0}px, ${0}px) rotate(${initialRot}deg)`;
+  grabbedCard.style.cursor = "grab";
 });
 
 // Throttle function (adjust interval as needed)
